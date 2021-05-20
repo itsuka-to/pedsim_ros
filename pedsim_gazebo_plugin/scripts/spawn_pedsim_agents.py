28ffceba50432269a97c707f28e0a093a6e21b8b
Tomoya Itsuka <50649356+itsuka-to@users.noreply.github.com>
2021-05-20 17:40:27 +0900

    modify spawn_pedsim_agents.py

--- pedsim_gazebo_plugin/scripts/spawn_pedsim_agents.py
+++ pedsim_gazebo_plugin/scripts/spawn_pedsim_agents.py
@@ -13,11 +13,13 @@ from geometry_msgs.msg import *
 from rospkg import RosPack
 from pedsim_msgs.msg  import AgentStates
 
+from ament_index_python.packages import get_package_share_directory
+
 # xml file containing a gazebo model to represent agent, currently is represented by a cubic but can be changed
 global xml_file
 
 def actor_poses_callback(actors):
-    global node
+    global node, xml_string
 
     for actor in actors.agent_states:
         actor_id = str( actor.id )
@@ -33,30 +35,30 @@ def actor_poses_callback(actors):
                                     actor_pose.orientation.w) )
 
         # cli.spawn_model(actor_id, xml_string, "", model_pose, "world")
-        spawn_entity(node, xml_string, model_pose)
+        spawn_entity(node, actor_id, xml_string, "", model_pose, "world")
     
     node.get_logger().info("all agents have been spawned !")
     node.destroy_node()
     rclpy.shutdown()
 
 
-def spawn_entity(node, entity_xml, initial_pose, timeout=DEFAULT_TIMEOUT):
+def spawn_entity(node, name, entity_xml, robot_namespace, initial_pose, reference_frame, gazebo_namespace="", timeout=DEFAULT_TIMEOUT):
     if timeout < 0:
         node.get_logger().error('spawn_entity timeout must be greater than zero')
         return False
     node.get_logger().info(
         'Waiting for service %s/spawn_entity, timeout = %.f' % (
-            node.args.gazebo_namespace, timeout))
-    node.get_logger().info('Waiting for service %s/spawn_entity' % node.args.gazebo_namespace)
-    client = node.create_client(SpawnEntity, '%s/spawn_entity' % node.args.gazebo_namespace)
+            gazebo_namespace, timeout))
+    node.get_logger().info('Waiting for service %s/spawn_entity' % gazebo_namespace)
+    client = node.create_client(SpawnEntity, '%s/spawn_entity' % gazebo_namespace)
     if client.wait_for_service(timeout_sec=timeout):
         req = SpawnEntity.Request()
-        req.name = node.args.entity
+        req.name = name
         req.xml = str(entity_xml, 'utf-8')
-        req.robot_namespace = node.args.robot_namespace
+        req.robot_namespace = robot_namespace
         req.initial_pose = initial_pose
-        req.reference_frame = node.args.reference_frame
-        node.get_logger().info('Calling service %s/spawn_entity' % node.args.gazebo_namespace)
+        req.reference_frame = reference_frame
+        node.get_logger().info('Calling service %s/spawn_entity' % gazebo_namespace)
         srv_call = client.call_async(req)
         while rclpy.ok():
             if srv_call.done():
@@ -70,17 +72,20 @@ def spawn_entity(node, entity_xml, initial_pose, timeout=DEFAULT_TIMEOUT):
 
 
 def main(args=args):
-    global node
+    global node, xml_string
     rclpy.init()
     
     node = rclpy.create_node('spawn_pedsim_agents')
     cli = node.create_client(SpawnEntity, "/spawn_entity")
 
-    rospack1 = RosPack()
-    pkg_path = rospack1.get_path('pedsim_gazebo_plugin')
-    default_actor_model_file = pkg_path + "/models/actor_model.sdf"
+    # rospack1 = RosPack()
+    # pkg_path = rospack1.get_path('pedsim_gazebo_plugin')
+    # default_actor_model_file = pkg_path + "/models/actor_model.sdf"
+    pkg_path = get_package_share_directory('pedsim_gazebo_plugin')
+    default_actor_model_file = pkg_path + "models/actor_model.sdf)
+    node.declare_parameter('~actor_model_file', default_actor_model_file)
 
-    actor_model_file = rospy.get_param('~actor_model_file', default_actor_model_file)
+    actor_model_file = node.get_parameter('~actor_model_file')
     file_xml = open(actor_model_file)
     xml_string = file_xml.read()
 
