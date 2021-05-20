6c24d04d4d34511b82603f6a9f2adea50240cef1
Tomoya Itsuka <50649356+itsuka-to@users.noreply.github.com>
2021-05-20 17:14:30 +0900

    modify pedsim_gazebo_ros

--- pedsim_gazebo_plugin/scripts/spawn_pedsim_agents.py
+++ pedsim_gazebo_plugin/scripts/spawn_pedsim_agents.py
@@ -5,8 +5,10 @@ Created on Mon Dec  2 17:03:34 2019
 @author: mahmoud
 """
 
-import rospy
-from gazebo_msgs.srv import SpawnModel
+import rclpy
+# from gazebo_msgs.srv import SpawnModel
+from gazebo_msgs.srv import SpawnEntity
+
 from geometry_msgs.msg import *
 from rospkg import RosPack
 from pedsim_msgs.msg  import AgentStates
@@ -15,10 +17,12 @@ from pedsim_msgs.msg  import AgentStates
 global xml_file
 
 def actor_poses_callback(actors):
+    global node
+
     for actor in actors.agent_states:
         actor_id = str( actor.id )
         actor_pose = actor.pose
-        rospy.loginfo("Spawning model: actor_id = %s", actor_id)
+        node.get_logger().info("Spawning model: actor_id = %s" % actor_id)
 
         model_pose = Pose(Point(x= actor_pose.position.x,
                                y= actor_pose.position.y,
@@ -28,15 +32,49 @@ def actor_poses_callback(actors):
                                     actor_pose.orientation.z,
                                     actor_pose.orientation.w) )
 
-        spawn_model(actor_id, xml_string, "", model_pose, "world")
-    rospy.signal_shutdown("all agents have been spawned !")
+        # cli.spawn_model(actor_id, xml_string, "", model_pose, "world")
+        spawn_entity(node, xml_string, model_pose)
+    
+    node.get_logger().info("all agents have been spawned !")
+    node.destroy_node()
+    rclpy.shutdown()
 
 
+def spawn_entity(node, entity_xml, initial_pose, timeout=DEFAULT_TIMEOUT):
+    if timeout < 0:
+        node.get_logger().error('spawn_entity timeout must be greater than zero')
+        return False
+    node.get_logger().info(
+        'Waiting for service %s/spawn_entity, timeout = %.f' % (
+            node.args.gazebo_namespace, timeout))
+    node.get_logger().info('Waiting for service %s/spawn_entity' % node.args.gazebo_namespace)
+    client = node.create_client(SpawnEntity, '%s/spawn_entity' % node.args.gazebo_namespace)
+    if client.wait_for_service(timeout_sec=timeout):
+        req = SpawnEntity.Request()
+        req.name = node.args.entity
+        req.xml = str(entity_xml, 'utf-8')
+        req.robot_namespace = node.args.robot_namespace
+        req.initial_pose = initial_pose
+        req.reference_frame = node.args.reference_frame
+        node.get_logger().info('Calling service %s/spawn_entity' % node.args.gazebo_namespace)
+        srv_call = client.call_async(req)
+        while rclpy.ok():
+            if srv_call.done():
+                node.get_logger().info('Spawn status: %s' % srv_call.result().status_message)
+                break
+            rclpy.spin_once(node)
+        return srv_call.result().success
+    node.get_logger().error(
+        'Service %s/spawn_entity unavailable. Was Gazebo started with GazeboRosFactory?')
+    return False
 
 
-if __name__ == '__main__':
-
-    rospy.init_node("spawn_pedsim_agents")
+def main(args=args):
+    global node
+    rclpy.init()
+    
+    node = rclpy.create_node('spawn_pedsim_agents')
+    cli = node.create_client(SpawnEntity, "/spawn_entity")
 
     rospack1 = RosPack()
     pkg_path = rospack1.get_path('pedsim_gazebo_plugin')
@@ -46,10 +84,22 @@ if __name__ == '__main__':
     file_xml = open(actor_model_file)
     xml_string = file_xml.read()
 
-    print("Waiting for gazebo services...")
-    rospy.wait_for_service("gazebo/spawn_sdf_model")
-    spawn_model = rospy.ServiceProxy("gazebo/spawn_sdf_model", SpawnModel)
-    print("service: spawn_sdf_model is available ....")
-    rospy.Subscriber("/pedsim_simulator/simulated_agents", AgentStates, actor_poses_callback)
 
-    rospy.spin()
+    node.get_logger().info("Waiting for gazebo services...")
+    while not cli.wait_for_service(timeout_sec=1.0):
+        node.get_logger().info('service not availabe, waiting again...')
+    
+    node.get_logger().info("service: spawn_sdf_model is available ....")
+
+    actor_poses_sub_ = node.create_subscription(
+        AgentStates,
+        "/pedsim_simulator/simulated_agents",
+        actor_poses_callback,
+        10
+    )
+
+    rclpy.spin(node)
+
+
+if __name__ == '__main__':
+    main()
